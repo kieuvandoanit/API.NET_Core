85f1c773005c5ed5915e6c127be14cab55bf1802
Kieu Van Doan <kieuvandoanit@gmail.com>
2021-04-07 18:06:42 +0700

    first commit

---
+++ StudentAPI/Controllers/StudentController.cs
@@ -0,0 +1,101 @@
+﻿using StudentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+        // GET api/<controller>
+        // decalre sql connection and command object there
+        private SqlConnection _conn;
+        private SqlDataAdapter _adapter;
+        public IEnumerable<Student> Get()
+        {
+            _conn = new SqlConnection("Data Source=.;Initial Catalog=Store;Integrated Security=True");
+            DataTable _dt = new DataTable();
+            var query = "Select * from student";
+            _adapter = new SqlDataAdapter
+            {
+                SelectCommand = new SqlCommand(query, _conn)
+
+            };
+            _adapter.Fill(_dt);
+            List<Student> students = new List<Models.Student>(_dt.Rows.Count);
+            if (_dt.Rows.Count > 0)
+            {
+                foreach(DataRow studentrecord in _dt.Rows)
+                {
+                    students.Add(new ReadStudent(studentrecord));
+                }
+            }
+            return students;
+        }
+
+        // GET api/<controller>/5
+        public IEnumerable<Student> Get(int id)
+        {
+
+            _conn = new SqlConnection("Data Source=.;Initial Catalog=Store;Integrated Security=True");
+            DataTable _dt = new DataTable();
+            var query = "Select * from student where Id =" + id;
+            _adapter = new SqlDataAdapter
+            {
+                SelectCommand = new SqlCommand(query, _conn)
+
+            };
+            _adapter.Fill(_dt);
+            List<Student> students = new List<Models.Student>(_dt.Rows.Count);
+            if (_dt.Rows.Count > 0)
+            {
+                foreach (DataRow studentrecord in _dt.Rows)
+                {
+                    students.Add(new ReadStudent(studentrecord));
+                }
+            }
+            return students;
+        }
+
+        // POST api/<controller>
+        public string Post([FromBody]createStudent value)
+        {
+            _conn = new SqlConnection("Data Source=.;Initial Catalog=Store;Integrated Security=True");
+
+            var query = "insert into student (f_name, m_name, l_name, address, birthdate, score) values(@f_name, @m_name, @l_name, @address, @birthDate, @score)";
+            SqlCommand insertCommand = new SqlCommand(query, _conn);
+            insertCommand.Parameters.AddWithValue("@f_name",value.f_name);
+            insertCommand.Parameters.AddWithValue("@m_name",value.m_name);
+            insertCommand.Parameters.AddWithValue("@l_name",value.l_name);
+            insertCommand.Parameters.AddWithValue("@address",value.address);
+            insertCommand.Parameters.AddWithValue("@birthdate",value.birthDate);
+            insertCommand.Parameters.AddWithValue("@score",value.score);
+
+            _conn.Open();
+            int result = insertCommand.ExecuteNonQuery();
+            if (result > 0)
+            {
+                return "true";
+            }
+            else
+            {
+                return "false";
+            }
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ StudentAPI/Models/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace StudentAPI.Models
+{
+    public class Student
+    {
+        //define all the properties of the table with get and set
+        
+    }
+    //will create a class to create an object of student class
+    public class createStudent : Student
+    {
+        public int Id { get; set; }
+        public string f_name { get; set; }
+        public string m_name { get; set; }
+        public string l_name { get; set; }
+        public string address { get; set; }
+        public string birthDate { get; set; }
+        public string score { get; set; }
+        public string dep_id { get; set; }
+    }
+    public class ReadStudent : Student
+    {
+        public ReadStudent(DataRow row)
+        {
+            Id = Convert.ToInt32(row["id"]);
+            f_name = row["f_name"].ToString();
+            m_name = row["m_name"].ToString();
+            l_name = row["l_name"].ToString();
+            address = row["address"].ToString();
+            birthDate = row["birthdate"].ToString();
+            score = row["score"].ToString();
+        }
+
+        public int Id { get; set; }
+        public string f_name { get; set; }
+        public string m_name { get; set; }
+        public string l_name { get; set; }
+        public string address { get; set; }
+        public string birthDate { get; set; }
+        public string score { get; set; }
+        public string dep_id { get; set; }
+    }
+
+}
